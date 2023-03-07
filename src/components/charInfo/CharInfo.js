53a57f442d9b4ffafc1a444f2df2ee1a0d0eeb32
Alexander Korobov <dktwho@mail.ru>
2023-03-07 15:24:58 +0300

    modified skeleton, img style for character without img, length of comics =9, and template text for character without list of comics

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -78,11 +78,16 @@ onError = () => {
 }
 
 const View = ({char}) => {
-  const {name, description, thumbnail, wiki, homepage, comics} = char
+  const {name, description, thumbnail, wiki, homepage, comics} = char;
+  let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
   return (
     <>
       <div className="char__basics">
-        <img src={thumbnail} alt="abyss"/>
+        <img src={thumbnail} alt="abyss" style={imgStyle} />
           <div>
               <div className="char__info-name">{name}</div>
               <div className="char__btns">
@@ -100,7 +105,9 @@ const View = ({char}) => {
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
+              {comics.length > 0 ? null : 'There is no comics with this character'}
               {comics.map((item, i) => {
+                if(i > 9) return;
                 return (
                   <li key={i} 
                       className="char__comics-item">
