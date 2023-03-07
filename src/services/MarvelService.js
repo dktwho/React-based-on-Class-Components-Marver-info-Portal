742234b26cb1832883d9e1e58208ea5b2e828bcf
Alexander Korobov <dktwho@mail.ru>
2023-03-07 10:37:01 +0300

    update component randomChar with render random character and modified MarvelService

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,7 +3,8 @@
 class MarvelService {
 
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=4a6f10ff8b34a7c84c34fc21f628bc72'
+  _apiKey = 'apikey=4a6f10ff8b34a7c84c34fc21f628bc72';
+
 
    getResources = async (url) => {
     let res = await fetch(url)
@@ -18,8 +19,19 @@ class MarvelService {
     return this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
   }
 
-  getCharacter = (id) => {
-    return this.getResources(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+  getCharacter = async (id) => {
+    const res = await this.getResources(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0])
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension ,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    }
   }
 }
 
